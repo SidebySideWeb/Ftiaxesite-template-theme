37cdd0e12b05da17a8a28fe14d7173da345b7c90
Dimitris Geronikolos <info@sports7.gr>
2025-10-20 16:36:56 +0300

    ci: let LHCI manage Next.js server, remove manual start/kill

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -3,8 +3,8 @@ module.exports = {
     collect: {
       url: ["http://localhost:3000"],
       startServerCommand: "npm run start",
-      startServerReadyPattern: "ready on",
-      numberOfRuns: 3,
+      startServerReadyPattern: "started server on|ready on|started on|Listening on|listening on|Ready on|ready at|started at|ready|listening",
+      numberOfRuns: 3
     },
     upload: {
       target: "temporary-public-storage",
